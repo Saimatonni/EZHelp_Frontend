1ebeed3b1c70b7183e1c2d80b9612e346573284a
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2024-04-25 23:52:08 +0600

    Client_LogIn

--- src/Pages/login-client.jsx
+++ src/Pages/login-client.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import '../styles/loginclient_style.css';
+import leftbg from "../assets/images/background/client_login.png";
+import logo from "../assets/images/brand/logo.jpg";
+import googlelogo from "../assets/images/brand/google_logo.png";
 
 const LoginClient = () => {
   return (
-    <div  className="h-full flex flex-col justify-center py-10 bg-cover bg-center bg-no-repeat">
-        LoginClient
+    <div className="h-full flex flex-col justify-center py-10 bg-cover bg-center bg-no-repeat">
+      <div className="login-container flex">
+        <div className="left-half bg-cover bg-center bg-no-repeat">
+          <img src={leftbg} alt="Left half image" className="w-full h-full object-cover object-top" />
         </div>
-  )
-}
+        <div className="right-half">
+          <div className="logo-container">
+            <img src={logo} alt="Logo" />
+          </div>
+          <div className="signin-text">
+            <h2>Sign in</h2>
+            <b>As Client</b>
+          </div>
+          <div className="signin-options">
+            <button className="google-signin">
+              <div className="google-signin-content">
+                <img src={googlelogo} alt="Google logo" />
+                <span>Sign in with Google</span>
+              </div>
+            </button>
+            <div className="or-text">
+              or
+            </div>
+            <button className="client-signin">
+              Sign in as a Service Provider
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LoginClient
+export default LoginClient;
