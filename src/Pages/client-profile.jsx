0830060cf57dbf63fa5820092434c356620909bb
NoushinTasnim <noushintasnim.2615@gmail.com>
2024-05-06 23:42:21 +0600

    sp_profile

--- src/Pages/client-profile.jsx
+++ src/Pages/client-profile.jsx
@@ -3,11 +3,15 @@ import gradientbg from "../assets/images/banner/vector2.png";
 
 const ClientProfile = () => {
   return (
-    <div
-      className="h-full flex flex-col justify-center p-10 bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${gradientbg})` }}
-    >
-      ClientProfile
+    <div className="container">
+      <div className="image-container">
+        <img src="your-image-url.jpg" alt="Circular Image" className="circular-image" />
+      </div>
+      <div className="text-container">
+        <input type="text" placeholder="Text Field 1" className="text-field" />
+        <input type="text" placeholder="Text Field 2" className="text-field" />
+        <input type="text" placeholder="Text Field 3" className="text-field" />
+      </div>
     </div>
   );
 };
---
+++ src/data/service_provided.js
@@ -0,0 +1,55 @@
+const service_provided = [
+    {
+      id: "320331",
+      name: "Donald Trump",
+      rating: 4.5,
+      start: "25/06/2023",
+      finish: "28/06/2023",
+      amount: "1500tk",
+      isFinished: "Finished",
+      image: "image1.png"
+    },
+    {
+        id: "320331",
+        name: "Donald Trump",
+        rating: 4.5,
+        start: "28/04/2024",
+        finish: "-",
+        amount: "2000tk",
+        isFinished: "In Progress",
+        image: "image1.png"
+      },
+      {
+        id: "320331",
+        name: "Donald Trump",
+        rating: 4.5,
+        start: "25/06/2023",
+        finish: "28/06/2023",
+        amount: "1500tk",
+        isFinished: "Yes",
+        image: "image1.png"
+      },
+      {
+        id: "320331",
+        name: "Donald Trump",
+        rating: 4.5,
+        start: "25/06/2023",
+        finish: "28/06/2023",
+        amount: "1500tk",
+        isFinished: "Yes",
+        image: "image1.png"
+      },
+      {
+        id: "320331",
+        name: "Donald Trump",
+        rating: 4.5,
+        start: "25/06/2023",
+        finish: "28/06/2023",
+        amount: "1500tk",
+        isFinished: "Yes",
+        image: "image1.png"
+      },
+  ];
+  
+  export default service_provided;
+  
